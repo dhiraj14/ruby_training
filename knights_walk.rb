f71af968edae16a911f177cb323630f95b125776
dhirajkumar-webonise <mdhiraj01@gmail.com>
2015-03-11 14:41:56 +0530

    knight_problem updated

--- knights_walk.rb
+++ knights_walk.rb
@@ -1,15 +1,21 @@
 
 def knight_tour(x_position,y_position)  
-  minimum,combination_no,deg,index_x,index_y=8,0,0,x_position,y_position
+  combination_no,deg,index_x,index_y=0,0,x_position,y_position
   if $moves==($chess_width)*($chess_length)
-    print_solution
-    puts "moves completed"
+    if $solution.include?(-1) 
+      puts "solution does not exist"
+      return
+    else
+      print_solution
+      puts "moves completed"
+    end
   else
+    minimum=8
     while combination_no<8
-      #puts($x_combinations[combination_no],$y_combinations[combination_no])
+    
       x_new = x_position+$x_combinations[combination_no]
       y_new = y_position+$y_combinations[combination_no]
-      if is_safe(x_new,y_new)==true
+      if check(x_new,y_new)==true
         deg=degree(x_new,y_new)
         if deg<minimum
           minimum=deg
@@ -18,9 +24,9 @@ def knight_tour(x_position,y_position)
       end
       combination_no+=1
     end 
-    puts($moves,index_x,index_y)
     $moves=$moves+1
     $solution[index_x][index_y]=$moves
+    
     knight_tour(index_x,index_y)  
   end
 end
@@ -32,7 +38,7 @@ def degree(x_position,y_position)
   while  combination_no< 8
     x_new = x_position+$x_combinations[combination_no]
     y_new = y_position+$y_combinations[combination_no]
-    if is_safe(x_new,y_new)
+    if check(x_new,y_new)
       degree+=1  
     end
     combination_no+=1
@@ -41,7 +47,7 @@ def degree(x_position,y_position)
 end
 
 
-def is_safe(x_new,y_new)
+def check(x_new,y_new)
   if x_new<$chess_length&&x_new>=0&&y_new<$chess_width&&y_new>=0&&$solution[x_new][y_new]==-1
     return true
   else
@@ -71,23 +77,12 @@ $chess_length=gets.chomp.to_i
 puts "enter the chess_width"
 $chess_width=gets.chomp.to_i	
 $solution = Array.new($chess_length){Array.new($chess_width,-1)}
-$moves=0
-
+$moves=1
+$solution[x_position][y_position] =1
 knight_tour(x_position,y_position)
 
 
 
 
-=begin
-  k = 0
-  while k < 8
-    x_new = x_position+$x_combinations[k]
-    y_new = y_position+$y_combinations[k]
-    if is_safe(x_new,y_new)
-      puts degree(x_new,y_new)
-    
-    end
-  k+=1
-  end
-=end
+
 
